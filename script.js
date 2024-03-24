1960a28f2100552ac5b42739e3a1043d343846b3
uckbu <johnsobalci@gmail.com>
2024-03-23 22:31:45 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -8,21 +8,49 @@ function typeWriter() {
     typingAnimation.innerHTML += name.charAt(index);
     index++;
     setTimeout(typeWriter, 200);
-    }
-    }
-    
-    document.addEventListener('DOMContentLoaded', typeWriter);
-    
-    // Burger Menu
-    const burgerMenu = document.querySelector('.burger-menu');
-    const menuLinks = document.querySelector('.menu-links');
-    
-    burgerMenu.addEventListener('click', function() {
-    this.classList.toggle('active');
-    });
-    
-    document.addEventListener('click', function(event) {
-    if (!burgerMenu.contains(event.target)) {
+  }
+}
+
+document.addEventListener('DOMContentLoaded', typeWriter);
+
+// Burger Menu
+const burgerMenu = document.querySelector('.burger-menu');
+const menuLinks = document.querySelector('.menu-links');
+
+burgerMenu.addEventListener('click', function() {
+  this.classList.toggle('active');
+});
+
+document.addEventListener('click', function(event) {
+  if (!burgerMenu.contains(event.target)) {
     burgerMenu.classList.remove('active');
-    }
+  }
+});
+
+// Smooth Scroll
+const menuItems = document.querySelectorAll('.menu-links a');
+
+menuItems.forEach(item => {
+  item.addEventListener('click', function(event) {
+    event.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    const topOffset = document.querySelector('header').offsetHeight;
+    const elementPosition = targetElement.offsetTop;
+    const offsetPosition = elementPosition - topOffset;
+
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth'
     });
+
+    burgerMenu.classList.remove('active');
+  });
+});
+
+
+    document.addEventListener('mousemove', function(e) {
+        var cursorCircle = document.getElementById('cursor-circle');
+        cursorCircle.style.left = e.clientX - 20 + 'px';
+        cursorCircle.style.top = e.clientY - 20 + 'px';
+      });
